6ad9a62e56dc334efc3eeb5b9ab551f43fd8567f
capnramses <lists@antongerdelan.net>
2015-11-02 21:03:33 +0000

    rendering loop in x11 demo

--- 017_x11_demo/main.c
+++ 017_x11_demo/main.c
@@ -1,10 +1,52 @@
 // clang -o test main.c -std=c99 -lX11
 
+// problems:
+// * load system fonts?
+// * tearing vsyc refresh rate
+// * window close does not quit / server times out
+
 #include <X11/Xlib.h>
 #include <stdio.h>
 #include <unistd.h> // sleep
 #include <stdbool.h>
 #include <string.h>
+
+void draw_frame (Display* display, Window window, GC graphics_context) {
+	if (!display) {
+		fprintf (stderr, "ERROR: lost display\n");
+		return;
+	}
+	
+	XClearWindow (display, window);
+	
+	XDrawLine (display, window, graphics_context, 10, 10, 1014, 758);
+
+	int x = 30, y = 10;
+	char txt[] = "Hello X11!";
+	int len = strlen (txt);
+	XDrawString (display, window, graphics_context, x, y, txt, len);
+	x = 30, y = 20;
+	char txtb[] = "Here is a lengthy treatise on fonts.";
+	len = strlen (txtb);
+	XDrawString (display, window, graphics_context, x, y, txtb, len);
+	
+	//XFlush (display); // dispatches the command queue
+}
+
+void event_loop (Display* display, Window window, GC graphics_context) {
+	while (true) {
+		if (!display) {
+			fprintf (stderr, "ERROR: lost display\n");
+			return;
+		}
+	
+		XEvent event;
+		XNextEvent (display, &event);
+		if (event.type == Expose) { // was MapNotify
+			draw_frame (display, window, graphics_context);
+		}
+	}
+}
 
 int main () {
 	printf ("X11 demo\n");
@@ -30,7 +72,7 @@ int main () {
 		window = XCreateSimpleWindow (display, DefaultRootWindow (display), 0, 0,
 			1024, 768, 0, black_colour, black_colour);
 		// get MapNotify events etc.
-		XSelectInput (display, window, StructureNotifyMask);
+		XSelectInput (display, window, ExposureMask); // was StructureNotifyMask
 		// put window on screen command
 		XMapWindow (display, window);
 		// create graphics context
@@ -40,15 +82,9 @@ int main () {
 	{ // draw
 		// set drawing colour
 		XSetForeground (display, graphics_context, white_colour);
-		while (true) {
-			XEvent event;
-			XNextEvent (display, &event);
-			if (event.type == MapNotify) {
-				break;
-			}
-		}
+		
 
-		XDrawLine (display, window, graphics_context, 10, 10, 1014, 758);
+		
 
 		// note: can also unload a font
 		const char* font_name = "-*-Monospace-*-10-*";
@@ -63,17 +99,7 @@ int main () {
 		}
 		XSetFont (display, graphics_context, font->fid);
 
-		int x = 30, y = 10;
-		char txt[] = "Hello X11!";
-		int len = strlen (txt);
-		XDrawString (display, window, graphics_context, x, y, txt, len);
-		x = 30, y = 20;
-		char txtb[] = "Here is a lengthy treatise on fonts.";
-		len = strlen (txtb);
-		XDrawString (display, window, graphics_context, x, y, txtb, len);
-
-		XFlush (display); // dispatches the command queue
-		sleep (10);
+		event_loop (display, window, graphics_context);
 	} // enddraw
 
 	return 0;
