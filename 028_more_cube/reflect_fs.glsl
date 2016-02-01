9a3cdb0380b0fec431851a82971cdfe98ac77b0b
anton gerdelan <lists@antongerdelan.net>
2016-02-01 15:07:50 +0000

    added self-shadowing

--- 028_more_cube/reflect_fs.glsl
+++ 028_more_cube/reflect_fs.glsl
@@ -1,22 +1,26 @@
 #version 410
 
-in vec3 pos_eye;
-in vec3 n_eye;
+in vec3 texcoords;
 uniform samplerCube cube_texture;
-uniform mat4 V; // view matrix
+uniform vec3 light_pos_wor;
 out vec4 frag_colour;
 
 void main () {
-	/* reflect ray around normal from eye to surface */
-	vec3 incident_eye = normalize (pos_eye);
-	vec3 normal = normalize (n_eye);
-
-	vec3 reflected = reflect (incident_eye, normal);
-	// convert from eye to world space
-	reflected = vec3 (inverse (V) * vec4 (reflected, 0.0));
-
-	vec3 texel = texture (cube_texture, reflected).rgb;
 	vec3 base = vec3 (0.1, 0.3, 0.3);
-	//vec3 c = mix (texel, base, 0.9);
 	frag_colour = vec4 (base, 1.0);
+
+	{ // shadow cube
+		float texel = texture (cube_texture, texcoords).r;
+		// note: texcoords are also world position of cube verts/frags
+
+		// TODO for self-shadows
+		// if length is CLOSER than own 1d distance to light -->shadow, else not
+		// and add bias
+		float bias = 0.025;
+		float l = length (texcoords - light_pos_wor);
+		if (texel + bias < l) {
+			frag_colour.rgb *= 0.2;
+		}
+
+	}
 }
