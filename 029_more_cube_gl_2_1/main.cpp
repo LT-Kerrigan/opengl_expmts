44077a00701f1bdbbb4eebac1665d84f3f13b7c6
Anton Gerdelan <lists@antongerdelan.net>
2016-02-07 10:39:26 +1300

    float extension works on mesa, 2.1 16 int didnt

--- 029_more_cube_gl_2_1/main.cpp
+++ 029_more_cube_gl_2_1/main.cpp
@@ -133,12 +133,12 @@ bool load_cube_map_side (GLuint texture, GLenum side_target,
 	glTexImage2D (
 		side_target,
 		0,
-		GL_RGBA16,
+		GL_RGBA32F,
 		g_cube_map_dims,
 		g_cube_map_dims,
 		0,
 		GL_RED, // think this is the right way around (didnt work swapped around)
-		GL_INT, // unsigned byte only goes from 0 to 255!
+		GL_FLOAT, // unsigned byte only goes from 0 to 255!
 		NULL
 	);
 
